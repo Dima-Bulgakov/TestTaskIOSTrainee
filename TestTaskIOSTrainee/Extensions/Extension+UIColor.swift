4a68bbd79f7d406d796a53387b424f97376460d5
Dima-Bulgakov <bulgakovdu@gmail.com>
2023-10-19 16:15:55 +0300

    add coolers and Extension+UIColor

---
+++ TestTaskIOSTrainee/Extensions/Extension+UIColor.swift
@@ -0,0 +1,15 @@
+//
+//  Extension+UIColor.swift
+//  TestTaskIOSTrainee
+//
+//  Created by Dima on 19.10.2023.
+//
+
+import UIKit
+
+extension UIColor {
+    static let background = UIColor(named: "background")
+    static let title = UIColor(named: "title")
+    static let text = UIColor(named: "text")
+    static let button = UIColor(named: "button")
+}
